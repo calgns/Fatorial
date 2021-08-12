3a139b227503bd9aae3d4537bac9f498c5ca6749
Matheus Henrique <matheus110903@gmail.com>
2021-08-12 11:32:16 -0300

    terminei de fazer o fatorial e adicionar comentarios

--- fac.py
+++ fac.py
@@ -9,5 +9,16 @@ def fatorial(numero):
     #veja que não é necessário o uso de else,
     # pois, apenas se numero for = 1 que vai retornar alguma coisa
     return numero * fatorial(numero - 1)
-    #agora oque o ²return esta fazendo é pegar um numero e multiplicar pelo numero - 1
-    # exemplo: 5 * (5 - 1)=4, ou seja, ele vai multiplicar 5 * 4. sempre um valor a menos;
+    #agora o que o ²return esta fazendo é pegar um numero e multiplicar pelo numero - 1
+    # exemplo: 5 * (5 - 1) = 5 * 4, ou seja, ele vai multiplicar 5 * 4. sempre um valor a menos;
+
+fac5=fatorial(5)
+print("função fac5:", fac5, "\nconfirmando resultado:", 5 * 4 * 3 * 2 * 1)
+fac4 = fatorial(4)
+print("\nfunção fac4:", fac4, "\nconfirmando resultado:",  4 * 3 * 2 * 1)
+fac7 = fatorial(7)
+print("\nfunção fac7:", fac7, "\nconfirmando resultado:",  7 * 6 * 5 * 4 * 3 * 2 * 1)
+# e pronto ai esta uma maneira de ser fazer o fatorial por mais que
+# eu prefira o fatorial usando o while
+# esse é mais rapido e facil de se fazer.
+
